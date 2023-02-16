0df802a1d86b7d836055c5d156754248237a1fbc
Bence Jenei <bencej9812@gmail.com>
2023-02-16 10:49:59 +0100

    Add replaceChildren to slider function

--- script.js
+++ script.js
@@ -6,25 +6,26 @@ function createGrid (rows, columns) {
     container.style.setProperty("--grid-cols", columns);
     for (let i = 0; i < (rows * columns); i++) {
         let cell = document.createElement("div");
-        // cell.innerText = (i + 1);
         container.appendChild(cell).className = "grid-item";
     }
 }
 
-//createGrid(8, 8)
-
-/*
+// This method created a custom grid based on the slider input
 const slider = document.getElementById("slider");
 slider.oninput = function () {
     const currentValue = slider.value;
+    // Deletes all children of the parent node
+    container.replaceChildren();
     createGrid(currentValue, currentValue);
     console.log(currentValue);
 }
- ̇*/
 
+
+ /*
  document.getElementById("form").onsubmit = function () {
     const quantity = document.getElementById("quantity").value;
     console.log(document.getElementById("quantity").value);
     createGrid(quantity, quantity);
 
  }
+ */
